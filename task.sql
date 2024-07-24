187b992a200567fda0be5339df447e4129219b80
elfredburger <elfredfredburger@gmail.com>
2024-07-24 23:09:24 +0200

    Solution

--- task.sql
+++ task.sql
@@ -11,7 +11,7 @@ START TRANSACTION;
 INSERT INTO Products (Name, Description, Price, WarehouseAmount)
 VALUES ('AwersomeProduct', 'Product Desctiption', 5, 42);
 
-UPDATE Products SET WareHouseAmount = WareHouseAmount -1 WHERE ID = 1;
+UPDATE Products SET WarehouseAmount = WarehouseAmount -1 WHERE ID = 1;
 
 INSERT INTO OrderItems(OrderID, ProductID, Count)
 VALUES (1,1,1);

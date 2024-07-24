59cec4636a0682f35c1bcf8f89d864ef30e473b3
elfredburger <elfredfredburger@gmail.com>
2024-07-24 22:53:25 +0200

    Solution

--- task.sql
+++ task.sql
@@ -6,10 +6,9 @@ VALUES ('AwersomeProduct', 'Product Desctiption', 5, 42);
 INSERT INTO Customers (FirstName, LastName, Email, Address)
 VALUES ('John', 'Dou', 'j@dou.ua', 'far, far away');
 
-START TRANSACTION; 
-
 INSERT INTO Orders (CustomerID, Date)
 VALUES (1, NOW());
+START TRANSACTION; 
 
 INSERT INTO OrderItems(OrderID, ProductID, Count)
 VALUES (1,1,1);

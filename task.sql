fb2ff26f1bf433b9f8557ad5f7303ca7511c097d
elfredburger <elfredfredburger@gmail.com>
2024-07-24 23:06:53 +0200

    Solution

--- task.sql
+++ task.sql
@@ -1,6 +1,5 @@
 USE ShopDB; 
 
-
 INSERT INTO Customers (FirstName, LastName, Email, Address)
 VALUES ('John', 'Dou', 'j@dou.ua', 'far, far away');
 
@@ -12,9 +11,11 @@ START TRANSACTION;
 INSERT INTO Products (Name, Description, Price, WarehouseAmount)
 VALUES ('AwersomeProduct', 'Product Desctiption', 5, 42);
 
+UPDATE Products SET WareHouseAmount = WareHouseAmount -1 WHERE ID = 1;
+
 INSERT INTO OrderItems(OrderID, ProductID, Count)
 VALUES (1,1,1);
 
-UPDATE Products SET WareHouseAmount = WareHouseAmount -1 WHERE ID = 1;
+
 
 COMMIT;
